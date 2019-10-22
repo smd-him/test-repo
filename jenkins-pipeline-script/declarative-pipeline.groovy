aacd4b6fe08e596373355b4b242671d3ee67109a
jaibapna <jai.bapna@insigniac.com>
2019-10-22 16:15:13 +0530

    user input

--- jenkins-pipeline-script/declarative-pipeline.groovy
+++ jenkins-pipeline-script/declarative-pipeline.groovy
@@ -1,9 +1,16 @@
-node("master") {
-	stage("first"){
-	echo "This is the pipeline script"
-	}
-	stage("second")
-	{
-		echo "this is second pipeline script"
-	}
-}
+// node("master") {
+// 	stage("first"){
+// 	echo "This is the pipeline script"
+// 	}
+// 	stage("second")
+// 	{
+// 		echo "this is second pipeline script"
+// 	}
+// }
+def buildParams = [
+    booleanParam(name: "run", defaultValue: true ,description: "run this job")
+]
+
+properties([
+    parameters(buildParams),
+])
