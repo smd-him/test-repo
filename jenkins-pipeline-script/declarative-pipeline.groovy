84cab26cb9db1f6465c342d1fecd8434a9bca465
jaibapna <jai.bapna@insigniac.com>
2019-10-22 15:58:23 +0530

    declarative pipeline

--- jenkins-pipeline-script/declarative-pipeline.groovy
+++ jenkins-pipeline-script/declarative-pipeline.groovy
@@ -2,22 +2,22 @@ pipeline {
 	agent any
 	stages {
 		stage ('build') {
-			...
+		echo "first build"
 		}
 		stage ('test: integration-&-quality') {
-			...
+		echo "test integraion"
 		}
 		stage ('test: functional') {
-			...
+		echo "test functional"
 		}
 		stage ('test: load-&-security') {
-			...
+		echo "test load"
 		}
 		stage ('approval') {
-			...
+		echo "approval"
 		}
 		stage ('deploy:prod') {
-			...
+		echo "deploy production"
 		}
 	}
 }
